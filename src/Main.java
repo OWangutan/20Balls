867d70f22359c34c262bd31c7c89e73b2e0ed3fe
OWangutan <brianwang40646@gmail.com>
2023-01-24 09:47:53 -0600

    added a comment

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 public class Main {
     public static void main(String[] agrs){
-<<<<<<< HEAD
-        //commet form intellJ
-=======
->>>>>>> origin/main
+        //comment form IntellJ
         JFrame myFrame = new JFrame();
         myFrame.setSize(500,500);
         BallPanel bPanel = new BallPanel();
