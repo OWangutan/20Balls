37e0d6fbba454bad072cc6facd92103b1e65b1af
BRIAN WANG <123483372+OWangutan@users.noreply.github.com>
2023-01-24 15:40:36 +0000

    none

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+public class Main {
+    public static void main(String[] agrs){
+        JFrame myFrame = new JFrame();
+        myFrame.setSize(500,500);
+        BallPanel bPanel = new BallPanel();
+        myFrame.add(bPanel);
+
+        myFrame.setVisible(true);
+    }
+}
